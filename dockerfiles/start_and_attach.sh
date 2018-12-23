5163e67d8a1820aedb1a8be7fbaa4ab67f17034c
pedromslopes <pms.lopes@campus.fct.unl.pt>
2018-12-23 22:08:40 +0000

    Updated docker files

--- dockerfiles/start_and_attach.sh
+++ dockerfiles/start_and_attach.sh
@@ -3,9 +3,8 @@
 NPROC=$(nproc)
 
 if [ "$NPROC" -ge 2 ]; then
-  trap "echo 'Shutting down' && /opt/antidote/bin/env stop" TERM
-  /opt/antidote/bin/env start && sleep 10 && tail -f /opt/antidote/log/console.log &
-  sleep 10 && cd /opt/AQL && make shell
+  trap "echo 'Shutting down' && /opt/AQL/bin/env stop" TERM
+  /opt/AQL/bin/env start && sleep 10 && tail -f /opt/AQL/log/console.log & wait ${!}
 else
   echo "You need at least 2 cpu cores in your Docker (virtual) machine to run this image!"
   echo "You have ${NPROC} processors"
